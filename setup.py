445f12352dea872decd6f350e64d43d8bdca9acf
ErJokeCode <eriksoloviev@gmail.com>
2025-07-05 19:00:13 +0400

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="orm_core",
-    version="0.0.2",
+    version="0.0.3",
     author="Erik Soloviev",
     author_email="eriksoloviev@gmail.com",
     description="ORM Core Library for FastAPI and SQLAlchemy",
